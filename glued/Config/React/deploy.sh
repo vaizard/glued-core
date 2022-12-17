37e6ee796dbd0c015f501dd10f14449ca2b0a668
Pavel Stratil <pavel@vaizard.org>
2022-12-17 17:27:15 +0000

    stupid ass react deployment

---
+++ glued/Config/React/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# TODO Git clone mrdati. nedelat, vyhodit do separe mikrosluzby
+pushd ../data
+git clone --depth=1 https://github.com/vaizard/glued-react
+pushd glued-react
+git pull
+cp -r ../../glued-core/.env ./.env
+npm install
+npm run build
+cp -r ./build/* ../../glued-core/public
+popd
+popd
