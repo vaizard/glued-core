30f16c2c647f889e514151b78984cdf2cffc16d8
Pavel Stratil <pavel@vaizard.org>
2023-03-15 23:50:08 +0000

    coding style improvements

--- glued/routes.php
+++ glued/routes.php
@@ -5,10 +5,11 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpNotFoundException;
 
+
 /**
  * Catch-all route to serve a 204 options response
  */
-$app->options('/{routes:.+}', function ($request, $response, $args) {
+$app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
     return $response->withStatus(204);
 })->setName('fallback_options');
 
@@ -27,6 +28,6 @@ foreach ($settings['routes'] as $name => $leaf) {
  * Catch-all route to serve a 404 Not Found page if none of the routes match
  * NOTE: make sure this route is defined last
  */
-$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function (Request $request, Response $response) {
     throw new HttpNotFoundException($request);
 })->setName('fallback_notfound');
