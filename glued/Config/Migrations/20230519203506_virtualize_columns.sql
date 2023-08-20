5e2ffb52ae26bce0a53b1b1d97230c67e0f9203a
Pavel Stratil <pavel@vaizard.org>
2023-08-20 15:42:28 +0000

    replace migrations with clean slate

--- glued/Config/Migrations/20230519203506_virtualize_columns.sql
+++
@@ -1,9 +0,0 @@
--- migrate:up
-
-ALTER TABLE `t_core_users` DROP `c_locale`;
-ALTER TABLE `t_core_users` ADD `c_active` tinyint(1) GENERATED ALWAYS AS (json_unquote(json_extract(`c_attr`,_utf8mb4'$.status.active'))) STORED COMMENT '[STORED] Account activity status';
-ALTER TABLE `t_core_users` ADD `c_locale` char(5) GENERATED ALWAYS AS (json_unquote(json_extract(`c_attr`,_utf8mb4'$."locale"'))) STORED COMMENT '[STORED] Preferred locale';
-
--- migrate:down
-
-ALTER TABLE `t_core_users` DROP `c_active`;
